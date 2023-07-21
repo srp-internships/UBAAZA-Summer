b3eb403c158b94bb5120ee13d7a09025956aec11
ubaydulloev7343344 <ubaydulloev.1104@gmal.com>
2023-07-21 10:33:16 +0500

    migration Categories

---
+++ Blazor Server WebAssemble/BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using BlazorEcommerce.Server.Data;
+using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+	
+		private readonly IProductService _productService;
+
+		public ProductController(IProductService productService)
+        {
+			_productService = productService;
+		}
+        [HttpGet]
+		public async Task<ActionResult<ServiceResponse<List<Product>>>> GetProduct()
+		{
+			var result=await _productService.GetProductsAsync(); 
+			return Ok(result);
+		}
+		[HttpGet("{productId}")]
+		public async Task<ActionResult<ServiceResponse<Product>>> GetProduct(int productId)
+		{
+			var result = await _productService.GetProductsAsync(productId);
+			return Ok(result);
+		}
+
+
+	}
+}
